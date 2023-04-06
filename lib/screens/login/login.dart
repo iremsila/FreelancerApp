b2c5cea119c400a8dce225252c76b052cfa1a0e2
iremsila <83502239+iremsila@users.noreply.github.com>
2023-04-06 18:49:43 +0300

    login

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,87 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import '../../layout/layout.dart';
+import '../../shared/resources/colors.dart';
+import 'login_body.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Login extends StatelessWidget {
+  const Login({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Login Screen'),
-            ),
-            body: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
-                    child: FlutterLogo(
-                      size: 40,
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(90.0),
-                        ),
-                        labelText: 'Email',
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(90.0),
-                        ),
-                        labelText: 'Password',
-                      ),
-                    ),
-                  ),
-                  Container(
-                      height: 80,
-                      padding: const EdgeInsets.all(20),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                        ),
-                        child: const Text('Log In'),
-                        onPressed: () {},
-                      )),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ),
-                ],
-              ),
-            )));
+    return AppLayOut(
+      header: 'Login',
+      dis: 'Hi, Welcome!',
+      fields: Container(
+          decoration: BoxDecoration(
+              color: AppColors.white, borderRadius: BorderRadius.circular(30)),
+          padding: const EdgeInsets.all(20),
+          child: LoginBody()),
+    );
   }
 }
---
+++ lib/screens/login/login_body.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../helper/proivder.dart';
+import '../../helper/validators.dart';
+import '../../shared/items/widgets.dart';
+import '../../shared/resources/colors.dart';
+import '../../shared/resources/fields.dart';
+import '../../shared/resources/styles.dart';
+import '../../utils/methods.dart';
+import '../register/signup.dart';
+
+class LoginBody extends StatefulWidget {
+  const LoginBody({super.key});
+
+  @override
+  State<LoginBody> createState() => _LoginBodyState();
+}
+
+class _LoginBodyState extends State<LoginBody> {
+  GlobalKey<FormState> key = GlobalKey();
+  bool isSignIn = false;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: key,
+      child: SizedBox(
+        child: Column(
+          children: [
+            AppTextFields(
+              hint: 'UserName',
+              validator: (name) {
+                return AppValidator.requiredField(name ?? '');
+              },
+            ),
+            freev(),
+            AppPassFields(
+              validator: (pass) {
+                return AppValidator.passFieldValidator(pass ?? '');
+              },
+            ),
+            freev(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  'Forget Password?',
+                  style: AppStyles.links(),
+                )
+              ],
+            ),
+            freev(v: 10),
+            buttons(
+                text: 'Login',
+                color: AppColors.primary,
+                onTap: () {
+                  if (key.currentState!.validate()) {
+                    //go to Home page
+                  }
+                }),
+            buttons(
+                text: 'SignUp',
+                color: Colors.green,
+                onTap: () {
+                  navigatePush(context,
+                      secondPage: ChangeNotifierProvider(
+                          create: (context) {
+                            return AppProvider();
+                          },
+                          child: const SignUp()));
+                }),
+            freev(),
+          ],
+        ),
+      ),
+    );
+  }
+}
