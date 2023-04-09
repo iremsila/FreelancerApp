394378b21af8ca7bb36032ec36ce9405a0d533d6
iremsila <83502239+iremsila@users.noreply.github.com>
2023-04-09 18:24:18 +0300

    ikinci düzenleme

---
+++ lib/screens/forgot_password.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+
+
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent[700],
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Text(
+              "Enter your email and we'll send you a password reset link to your email. ",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white70,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextButton(
+            onPressed: (){},
+            child: Text("Password Reset"),
+          ),
+        ],
+      ),
+    );
+  }
+}
