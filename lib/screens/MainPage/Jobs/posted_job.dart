424b1f4820505064cba45bd10b60f0b018914e45
melikesoygullucu <melikesoygul@hotmail.com>
2023-05-18 11:29:17 +0300

    sixth commit

---
+++ lib/screens/MainPage/Jobs/posted_job.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:mysql1/mysql1.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class JobListScreen extends StatefulWidget {
+  @override
+  _JobListScreenState createState() => _JobListScreenState();
+}
+
+class _JobListScreenState extends State<JobListScreen> {
+  List<Map<String, dynamic>> jobList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromDatabase();
+  }
+
+  Future<void> fetchDataFromDatabase() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int userId = prefs.getInt('userId') ?? 0;
+
+    final conn = await MySqlConnection.connect(ConnectionSettings(
+      host: '213.238.183.81',
+      port: 3306,
+      user: 'httpdegm_hudai',
+      password: ',sPE[gd^hbl1',
+      db: 'httpdegm_database1',
+    ));
+
+    final jobResult = await conn.query(
+      'SELECT * FROM upload_job WHERE id = ?',
+      [userId],
+    );
+
+    if (jobResult.isNotEmpty) {
+      setState(() {
+        jobList = jobResult.map((row) => row.fields).toList();
+      });
+    }
+
+    await conn.close();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Job You Posted'),
+      ),
+      body: ListView.builder(
+        itemCount: jobList.length,
+        itemBuilder: (context, index) {
+          final job = jobList[index];
+          return ListTile(
+            title: Text(job['job_title']),
+            subtitle: Text(job['category']),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => JobDetailScreen(job: job),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class JobDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> job;
+
+  const JobDetailScreen({required this.job});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Job Detail'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Job Title: ${job['job_title']}',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text('Category: ${job['category']}'),
+            SizedBox(height: 8.0),
+            Text('Location: ${job['location']}'),
+            SizedBox(height: 8.0),
+            Text('Description: ${job['description']}'),
+            SizedBox(height: 8.0),
+            Text('Budget: ${job['budget']}'),
+            SizedBox(height: 8.0),
+            Text('Date Posted: ${job['date_posted']}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
